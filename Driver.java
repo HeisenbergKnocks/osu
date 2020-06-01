a48e4b5710d09c8d7955c4a0472a0876cab446b3
VisionistJavascript <33534268+VisionistJavascript@users.noreply.github.com>
2020-06-01 11:02:37 -0700

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -24,6 +24,9 @@ public class Driver extends JPanel implements ActionListener, KeyListener, Mouse
 	
 	int responseTime = 75;
 	int circleSize = 100;
+	
+	Font numberFont = new Font("Arial",0,50);
+	Font scoreFont = new Font("Arial",0,20);
 	//keys
 	boolean keys[] = new boolean[256];
 	int mouseX = 0;
@@ -34,12 +37,13 @@ public class Driver extends JPanel implements ActionListener, KeyListener, Mouse
 	
 	@Override
 	public void paint(Graphics g) {
-		super.paintComponent(g);
-		
+	super.paintComponent(g);
+	
 		// background
 		g.setColor(new Color(0, 0, 0));
 		g.fillRect(0, 0, 2000, 1600);
-
+		
+		g.setFont(scoreFont);
 		g.setColor(new Color(255,255,255));
 		g.drawString("score: "+score, 25, 25);
 		
@@ -47,19 +51,23 @@ public class Driver extends JPanel implements ActionListener, KeyListener, Mouse
 		if (press) {
 			g.setColor(new Color(100,100,100));
 		}
-    //circles
-    for (int i = 0; i < circles.size(); i++) {
-			Circle s = circles.get(i);
-			//skip premature circles
-			if (s.startTime>tick+responseTime) continue;
-			if (s.finished) continue;
-			
-			if (s.startTime-tick>0)
-				g.drawOval(s.getX()-circleSize/2-(s.startTime-tick)/2, s.getY()-circleSize/2-(s.startTime-tick)/2, circleSize+s.startTime-tick, circleSize+s.startTime-tick);
-
-			g.drawOval(s.getX()-circleSize/2, s.getY()-circleSize/2, circleSize, circleSize);
+	
+		g.setFont(numberFont);
+		//circles
+	    for (int i = 0; i < circles.size(); i++) {
+				Circle s = circles.get(i);
+				//skip premature and finished circles
+				if (s.startTime>tick+responseTime) continue;
+				if (s.finished) continue;
+				
+				if (s.startTime-tick>0)
+					g.drawOval(s.getX()-circleSize/2-(s.startTime-tick)/2, s.getY()-circleSize/2-(s.startTime-tick)/2, circleSize+s.startTime-tick, circleSize+s.startTime-tick);
+	
+				g.drawOval(s.getX()-circleSize/2, s.getY()-circleSize/2, circleSize, circleSize);
+				
+				g.drawString(""+s.number, s.getX()-12, s.getY()+12);
 		}
-    //sliders
+	    //sliders
 		for (int i = 0; i < sliders.size(); i++) {
 			Slider s = sliders.get(i);
 			//skip premature circles
@@ -71,7 +79,7 @@ public class Driver extends JPanel implements ActionListener, KeyListener, Mouse
 			
 			for (int j = 0; j < s.getX().length; j++) {
 				if (j<s.getX().length-1)
-					g.drawLine(s.getX()[j]-circleSize/2, s.getY()[j]-circleSize/2, s.getX()[j+1]-circleSize/2, s.getY()[j+1]-circleSize/2);
+					g.drawLine(s.getX()[j], s.getY()[j], s.getX()[j+1], s.getY()[j+1]);
 				g.drawOval(s.getX()[j]-circleSize/2, s.getY()[j]-circleSize/2, circleSize, circleSize);
 			}
 			g.drawOval(s.getCircleX()-circleSize/2, s.getCircleY()-circleSize/2, circleSize, circleSize);
@@ -144,43 +152,45 @@ public class Driver extends JPanel implements ActionListener, KeyListener, Mouse
 
 		//the song starts at t=100
 		//intro
-		circles.add(new Circle(400,400,100));
-		circles.add(new Circle(500,300,250));
-		circles.add(new Circle(400,300,300));
-		circles.add(new Circle(500,200,350));
-		circles.add(new Circle(100,200,400));
+		circles.add(new Circle(400,400,100,1));
+		circles.add(new Circle(500,300,250,2));
+		circles.add(new Circle(400,300,300,3));
+		circles.add(new Circle(500,200,350,4));
+		circles.add(new Circle(100,200,400,5));
 		
-		circles.add(new Circle(400,400,475));
-		circles.add(new Circle(500,300,525));
-		circles.add(new Circle(400,300,575));
-		circles.add(new Circle(500,200,625));
-		circles.add(new Circle(100,200,675));
-		circles.add(new Circle(200,200,725));
-		circles.add(new Circle(300,200,775));
-		circles.add(new Circle(400,400,825));
+		circles.add(new Circle(400,400,475,1));
+		circles.add(new Circle(500,300,525,2));
+		circles.add(new Circle(400,300,575,3));
+		circles.add(new Circle(500,200,625,4));
+		circles.add(new Circle(100,200,675,5));
+		circles.add(new Circle(200,200,725,6));
+		circles.add(new Circle(300,200,775,7));
+		circles.add(new Circle(400,400,825,8));
 		
 		//theme 1 part 1
-		circles.add(new Circle(100,200,950));
-		sliders.add(new Slider(new int[] {250,350}, new int[] {200,200},975));
-		circles.add(new Circle(500,200,1025));
-		circles.add(new Circle(400,300,1050));
-		circles.add(new Circle(500,400,1075));
-		circles.add(new Circle(400,500,1100));
+		circles.add(new Circle(100,200,950,1));
+		sliders.add(new Slider(new int[] {250,350}, new int[] {200,200},975, 2));
+		circles.add(new Circle(500,200,1025,4));
+		circles.add(new Circle(400,300,1050,5));
+		circles.add(new Circle(500,400,1075,6));
+		circles.add(new Circle(400,500,1100,7));
 		
-		circles.add(new Circle(100,600,950+175));
-		sliders.add(new Slider(new int[] {250,350}, new int[] {600,600},975+175));
-		circles.add(new Circle(500,600,1025+175));
-		circles.add(new Circle(400,500,1050+175));
-		circles.add(new Circle(500,400,1075+175));
-		circles.add(new Circle(400,300,1100+175));
+		circles.add(new Circle(100,600,950+175,1));
+		sliders.add(new Slider(new int[] {250,350}, new int[] {600,600},975+175,2));
+		circles.add(new Circle(500,600,1025+175,4));
+		circles.add(new Circle(400,500,1050+175,5));
+		circles.add(new Circle(500,400,1075+175,6));
+		circles.add(new Circle(400,300,1100+175,7));
 		
 		//theme 1 part 2
-		circles.add(new Circle(100,200,1350));
-		sliders.add(new Slider(new int[] {250,350}, new int[] {200,200},1375));
-		circles.add(new Circle(500,200,1425));
-		circles.add(new Circle(400,300,1450));
-		circles.add(new Circle(500,400,1475));
-		circles.add(new Circle(400,500,1500));
+		circles.add(new Circle(100,200,1350,1));
+		sliders.add(new Slider(new int[] {250,350}, new int[] {200,200},1375,2));
+		circles.add(new Circle(500,200,1425,4));
+		circles.add(new Circle(400,300,1450,5));
+		circles.add(new Circle(500,400,1475,6));
+		circles.add(new Circle(400,500,1500,7));
+		
+		//sliders.add(new Slider(new int[] {250,350,300}, new int[] {200,200,300}, 1550));
 		
 		
 		//TEMPLATES
